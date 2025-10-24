768c3eba5bca25ca04ce29d60d2b5227b2af5393
Timofey-Byunenko1 <byunenkotimofey@gmail.com>
2025-10-24 09:33:15 +0300

    add index file

---
+++ main.js
@@ -0,0 +1,10 @@
+const age = 16;
+var Name = "Tima"
+let Legal = false
+const IQ = null;
+const wow = 
+
+console.log(typeof age , Legal, IQ, );
+console.log(typeof Name);
+
+console.log(typeof wow);
