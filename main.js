31d2a1a81081dce177c1ff2e712ca027148b3358
Timofey-Byunenko1 <byunenkotimofey@gmail.com>
2025-10-24 19:48:37 +0300

    add index file

--- main.js
+++ main.js
@@ -7,4 +7,29 @@ const wow =
 console.log(typeof age , Legal, IQ, );
 console.log(typeof Name);
 
-console.log(typeof wow);
+console.log(typeof wow); 
+
+
+
+
+let ag = 16;
+let Iq = 20;
+let heigth = 17;
+
+
+
+
+let person = {
+age: ag,
+Iq: Iq,
+heigth: heigth
+}
+
+console.log(person);
+
+
+
+const number = 10369;
+
+const exit = number.toString().split('')
+console.log(exit);
